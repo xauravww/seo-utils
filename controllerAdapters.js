978b2ed8a875269ff0c8c9bb380b07d49cfd7221
xauravww <sauravmaheshwari8@gmail.com>
2025-07-04 18:27:02 +0530

    Add TumblrAdapter for publishing posts to Tumblr, enhancing controllerAdapters with new functionality. Update adapter map to route 'tumblr.com' and 'blog/tumblr' domains to the new adapter, improving website classification and publication capabilities. Include OAuth integration for authentication and basic markdown to HTML conversion for content formatting.

--- controllerAdapters.js
+++ controllerAdapters.js
@@ -16,6 +16,7 @@ import { sendTweet } from './controllers/social_media/twitterController.js';
 import { postToFacebook } from './controllers/social_media/facebookController.js';
 import { postToInstagram } from './controllers/social_media/instagramController.js';
 import { UBookmarkingAdapter } from './controllers/bookmarking/ubookmarkingController.js';
+import { OAuth } from 'oauth';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -1530,6 +1531,82 @@ class HashnodeAdapter extends BaseAdapter {
     }
 }
 
+// --- Tumblr Adapter ---
+class TumblrAdapter extends BaseAdapter {
+    constructor(args) {
+        super(args);
+        // Expect credentials in this.website.credentials
+        this.consumerKey = this.website.credentials.consumerKey;
+        this.consumerSecret = this.website.credentials.consumerSecret;
+        this.accessToken = this.website.credentials.accessToken;
+        this.accessTokenSecret = this.website.credentials.accessTokenSecret;
+        this.blogHostname = this.website.credentials.blogHostname; // e.g., 'myblog.tumblr.com'
+    }
+
+    async publish() {
+        this.log(`[EVENT] Entering TumblrAdapter publish method for ${this.blogHostname}.`, 'info', true);
+        if (!this.consumerKey || !this.consumerSecret || !this.accessToken || !this.accessTokenSecret || !this.blogHostname) {
+            const errorMessage = 'Missing Tumblr credentials (consumerKey, consumerSecret, accessToken, accessTokenSecret, blogHostname).';
+            this.log(errorMessage, 'error', true);
+            return { success: false, error: errorMessage };
+        }
+        const oauth = new OAuth(
+            'https://www.tumblr.com/oauth/request_token',
+            'https://www.tumblr.com/oauth/access_token',
+            this.consumerKey,
+            this.consumerSecret,
+            '1.0A',
+            null,
+            'HMAC-SHA1'
+        );
+        // Convert markdown to HTML for Tumblr
+        let bodyContent = this.content.body || this.content.markdown || this.content.html || '';
+        if (this.content.markdown) {
+            // Basic markdown to HTML conversion (headings, bold, italic, links, line breaks)
+            bodyContent = bodyContent
+                .replace(/^###### (.*)$/gm, '<h6>$1</h6>')
+                .replace(/^##### (.*)$/gm, '<h5>$1</h5>')
+                .replace(/^#### (.*)$/gm, '<h4>$1</h4>')
+                .replace(/^### (.*)$/gm, '<h3>$1</h3>')
+                .replace(/^## (.*)$/gm, '<h2>$1</h2>')
+                .replace(/^# (.*)$/gm, '<h1>$1</h1>')
+                .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+                .replace(/__(.*?)__/g, '<strong>$1</strong>')
+                .replace(/\*(.*?)\*/g, '<em>$1</em>')
+                .replace(/_(.*?)_/g, '<em>$1</em>')
+                .replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>')
+                .replace(/\n/g, '<br/>');
+        }
+        const postData = {
+            type: 'text',
+            title: this.content.title || 'Untitled',
+            body: bodyContent
+        };
+        const url = `https://api.tumblr.com/v2/blog/${this.blogHostname}/post`;
+        return new Promise((resolve) => {
+            oauth.post(url, this.accessToken, this.accessTokenSecret, postData, (err, data) => {
+                if (err) {
+                    this.log('Failed to post to Tumblr: ' + JSON.stringify(err), 'error', true);
+                    resolve({ success: false, error: err });
+                } else {
+                    this.log('Successfully created Tumblr post: ' + data, 'success', true);
+                    // Try to extract the post URL from the response
+                    let postUrl = null;
+                    try {
+                        const parsed = JSON.parse(data);
+                        if (parsed && parsed.response && parsed.response.id && this.blogHostname) {
+                            postUrl = `https://${this.blogHostname}/post/${parsed.response.id}`;
+                        }
+                    } catch (e) {
+                        this.log('Failed to parse Tumblr response: ' + e.message, 'error', true);
+                    }
+                    resolve({ success: true, postUrl, response: data });
+                }
+            });
+        });
+    }
+}
+
 // Adapter map declaration moved above getAdapter to fix ReferenceError
 const adapterMap = {
     '../controllers/wpPostController.js': WordPressAdapter,
@@ -1551,6 +1628,8 @@ const adapterMap = {
     'blog/devto': DevToAdapter,
     'hashnode': HashnodeAdapter,
     'blog/hashnode': HashnodeAdapter,
+    'tumblr.com': TumblrAdapter,
+    'blog/tumblr': TumblrAdapter,
 };
 
 export const getAdapter = (jobDetails) => {
